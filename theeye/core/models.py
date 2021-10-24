2a3ded61c0ffbba9ad378538e0b45ea43b68ac0a
Barth <juniior.barth@gmail.com>
2021-10-24 19:52:39 -0300

    Implements events

--- theeye/core/models.py
+++ theeye/core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Event(models.Model):
+    session_id = models.UUIDField()
+    category = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
+    data = models.JSONField()
+    timestamp = models.DateTimeField()
