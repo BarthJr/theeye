cd54180e47e1bcb2f0f7c76b0d094323c4b817fb
Barth <juniior.barth@gmail.com>
2021-10-24 20:07:32 -0300

    Now endpoint to create an event returns 202

--- theeye/core/views.py
+++ theeye/core/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets
+from rest_framework import status, viewsets
+from rest_framework.response import Response
 
 from .models import Event
 from .serializers import EventSerializer
@@ -7,3 +8,13 @@ from .serializers import EventSerializer
 class EventViewSet(viewsets.ModelViewSet):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
+
+    def create(self, request, *args, **kwargs):
+        self._create_event(request.data)
+
+        return Response(status=status.HTTP_202_ACCEPTED)
+
+    def _create_event(self, data):
+        serializer = EventSerializer(data=data)
+        serializer.is_valid()
+        serializer.save()
