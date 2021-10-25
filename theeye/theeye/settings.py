164824170fb571534ce757787d7e5a332763f6bd
Barth <juniior.barth@gmail.com>
2021-10-25 00:18:17 -0300

    Sets up filters

--- theeye/theeye/settings.py
+++ theeye/theeye/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'celery',
+    'django_filters',
     'core',
 ]
 
@@ -128,6 +129,7 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 100
 }
