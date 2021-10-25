7b00ab0ab811c65dbaf453a79a45d64e0a62e76a
Barth <juniior.barth@gmail.com>
2021-10-25 00:24:31 -0300

    Implements filters

---
+++ theeye/core/filters.py
@@ -0,0 +1,16 @@
+import django_filters
+
+from .models import Event
+
+
+class EventFilter(django_filters.FilterSet):
+    session_id = django_filters.CharFilter(lookup_expr='icontains')
+    category = django_filters.CharFilter(lookup_expr='icontains')
+    name = django_filters.CharFilter(lookup_expr='icontains')
+    start_date = django_filters.DateFilter(field_name='timestamp', lookup_expr='gt', )
+    end_date = django_filters.DateFilter(field_name='timestamp', lookup_expr='lt', )
+    timestamp_range = django_filters.DateRangeFilter(field_name='timestamp')
+
+    class Meta:
+        model = Event
+        fields = ('session_id', 'category', 'name')
--- theeye/core/views.py
+++ theeye/core/views.py
@@ -3,12 +3,14 @@ from rest_framework.response import Response
 
 from .tasks import create_event
 from .models import Event, EventError
+from .filters import EventFilter
 from .serializers import EventSerializer, EventErrorSerializer
 
 
 class EventViewSet(viewsets.ModelViewSet):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
+    filter_class = EventFilter
 
     def create(self, request, *args, **kwargs):
         create_event.delay(request.data)
