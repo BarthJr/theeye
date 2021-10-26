b548fa41ce3149b392ea0532db7133dbfcc8000b
Barth <juniior.barth@gmail.com>
2021-10-25 21:22:18 -0300

    Implements ordering by date

--- theeye/core/models.py
+++ theeye/core/models.py
@@ -8,8 +8,14 @@ class Event(models.Model):
     data = models.JSONField()
     timestamp = models.DateTimeField()
 
+    class Meta:
+        ordering = ['-timestamp']
+
 
 class EventError(models.Model):
     data = models.TextField()
     messages = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
